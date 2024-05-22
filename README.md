e4e732df31655ae57960493b5bcba5cdbb3acf82
Cliente CONTROL-PC <CltControl@GGIECLB3WRK065.espol.edu.ec>
2024-05-22 12:42:51 -0500

    Cambio ari

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # prueba329
+
+Arianna Feijoo
+![Alt text](image.png)
--- TopMusical/src/main/java/espol/poo/topmusical/PrimaryController.java
+++ TopMusical/src/main/java/espol/poo/topmusical/PrimaryController.java
@@ -5,6 +5,7 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
@@ -31,7 +32,7 @@ public class PrimaryController {
 
     public void initialize() {
         listaCanciones = Cancion.leerCanciones();
-        Collections.sort(listaCanciones);
+        Collections.sort(listaCanciones,Comparator.reverseOrder());
         for (Cancion c : listaCanciones ) {
 
             HBox hb = new HBox(10);//hbox para ubicar info de cada cancion
